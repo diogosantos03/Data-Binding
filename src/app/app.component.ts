14e0fff30129d266ccca697e2bea948f4bb58f52
Diogo Santos <diogo.santos@dce.ufpb.br>
2018-02-16 17:19:33 -0300

    Life-Cycle do componente - Aula 15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  valor: number = 25;
+  deletarCiclo: boolean = false;
+
+  mudarValor(){
+    this.valor++;
+  }
+  quebrarCiclo(){
+    this.deletarCiclo = true;
+  }
+
 }
---
+++ src/app/ciclo/ciclo.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit,ngOnInit,ngOnChanges, ngDoCheck,
+ngAfterContentInit, ngAfterContentChecked, ngAfterViewInit,
+ ngAfterViewChecked,ngOnDestroy, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo',
+  templateUrl: './ciclo.component.html',
+  styleUrls: ['./ciclo.component.css']
+})
+export class CicloComponent implements OnInit,ngOnInit,ngOnChanges, ngDoCheck,
+ngAfterContentInit, ngAfterContentChecked, ngAfterViewInit, ngAfterViewChecked,ngOnDestroy{
+
+  @Input() valorInicial: number = 10;
+
+
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+  ngOnChanges(){
+    console.log('gnOnChanges');
+  }
+
+  ngDoCheck(){
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit(){
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(){
+    this.log('ngOnDestroy');
+  }
+
+  private log(hook: string){
+    console.log(hook);
+  }
+
+}
